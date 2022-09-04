9c2db5c980b54933d0f1cf7aaaf11f736fed47a8
Hilst <hilstfelipe@gmail.com>
2022-09-03 22:53:18 -0300

    refactor Program.cs with swagger

--- Program.cs
+++ Program.cs
@@ -1,15 +1,35 @@
 
+using Microsoft.AspNetCore.Rewrite;
 using storage;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-//mapeando comportamento padrao do aspnet mvc
-app.MapGet("/", () => "Hello World!");
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    var rewriteOption = new RewriteOptions();
+    rewriteOption.AddRedirect("^$", "swagger");
+    app.UseRewriter(rewriteOption);
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
+
