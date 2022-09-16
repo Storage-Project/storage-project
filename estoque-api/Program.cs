c0a163c8db3db70bf06a0ad7ac046b75f822d9fb
Hilst <hilstfelipe@gmail.com>
2022-09-16 11:33:51 -0300

    adding auto migrations on startup

--- estoque-api/Program.cs
+++ estoque-api/Program.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.AspNetCore.Rewrite;
+using Microsoft.EntityFrameworkCore;
 using storage;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dataContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
