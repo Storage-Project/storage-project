b59928b26a331dd7e9dd08944f3c7a8ff91b636c
Hilst <hilstfelipe@gmail.com>
2022-09-19 20:20:50 -0300

    refactor startup by code

--- storage_app/App.xaml.cs
+++ storage_app/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using storage_app;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,12 @@ namespace wpf_demo_course
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
