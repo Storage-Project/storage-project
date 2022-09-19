5a9203ccb121e90ed32ee2eeb318149fa2e945ec
BiancaPuertaRocha <biapuertarocha@gmail.com>
2022-09-19 08:39:50 -0300

    starting app project

---
+++ storage_app/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using storage_app.Models;
+
+namespace storage_app.Controllers
+{
+    internal class ProductController
+    {
+        private string _baseUrl = "https://estoque-api.azurewebsites.net";
+
+        public async Task<List<Product>> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage Res = await client.GetAsync("v1/products");
+                if (Res.IsSuccessStatusCode)
+                {
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    products = JsonConvert.DeserializeObject<List<Product>>(EmpResponse);
+
+                    return products;
+                }
+
+            }
+            return null;
+        }
+    }
+}
---
+++ storage_app/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace storage_app.Models
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public Category Category { get; set; } = new();
+        public DateTimeOffset Create_at { get; set; } = DateTimeOffset.Now;
+    }
+}
