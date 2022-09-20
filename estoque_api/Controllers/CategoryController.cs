e8be7e7c61e72b8db2b8e1f08dc64cedadbbc213
BiancaPuertaRocha <biapuertarocha@gmail.com>
2022-09-19 21:58:07 -0300

    final endpoints

--- estoque_api/Controllers/CategoryController.cs
+++ estoque_api/Controllers/CategoryController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 
 using storage.Exceptions;
 using storage.Repository;
+using storage.Models;
 
 namespace storage.Controllers
 {
@@ -35,6 +36,78 @@ namespace storage.Controllers
             {
                 return StatusCode(500);
             }
+        }
+
+        [HttpGet]
+        [Route("categories/{id}")]
+        public async Task<IActionResult> GetAsync([FromRoute] int id)
+        {
+            try
+            {
+                var category = await _categoryRepository.GetCategoryByID(id);
+                if (category == null)
+                    return NoContent();
+                return Ok(category);
+            }
+            catch (InternalServerError)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost("categories")]
+        public async Task<IActionResult> PostAsync([FromBody] Category category)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            try
+            {
+                var cat = await _categoryRepository.InsertCategory(category);
+                return Created($"v1/categories/{cat.Id}", cat);
+            }
+            catch (InternalServerError)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPut("categories/{id}")]
+        public async Task<IActionResult> PutAsync([FromServices] AppDbContext context, [FromBody] Category category, [FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            try
+            {
+                var cat = await _categoryRepository.UpdateCategory(category, id);
+                if (cat == null)
+                    return NotFound();
+                return Ok(cat);
+            }
+            catch (InternalServerError)
+            {
+                return StatusCode(500);
+            }
+        }
+
+         [HttpDelete("categories/{id}")]
+        public async Task<IActionResult> DeleteAsync([FromServices] AppDbContext context, [FromRoute] int id)
+        {
+            var result = false;
+            try
+            {
+                result = await _categoryRepository.DeleteCategory(id);
+            }
+            catch (InternalServerError)
+            {
+                return StatusCode(500);
+            }
+
+            if (result == true)
+                return Ok();
+            else
+                return NotFound();
         }
     }
 }
