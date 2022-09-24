1d6932595e34407a1cf8a6d7bb4fd726374a2db2
Hilst <hilstfelipe@gmail.com>
2022-09-24 15:36:06 -0300

    create search object

---
+++ storage_app/Utils/Objects/Filter.cs
@@ -0,0 +1,15 @@
+﻿using storage_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace storage_app.Utils.Objects
+{
+    internal class Filter
+    {
+        public Category Category { get; set; } = new();
+        public string Description { get; set; } = string.Empty;
+    }
+}
