fedec6747803ab2bde9e1431eac500dbd8b0bce5
Hilst <hilstfelipe@gmail.com>
2022-09-24 15:31:46 -0300

    command base

---
+++ storage_app/Utils/CommandExecutor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace storage_app.Utils
+{
+    internal class CommandExecutor : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter)
+        {
+
+            Action(parameter);
+        }
+
+        private readonly Action<object?> Action;
+        public CommandExecutor(Action<object?> action)
+        {
+            Action = action;
+        }
+    }
+}
