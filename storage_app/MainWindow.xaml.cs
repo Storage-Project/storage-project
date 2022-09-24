5c5072e655c23444c11d4bec6e6bd50f843d3f41
Hilst <hilstfelipe@gmail.com>
2022-09-23 23:09:44 -0300

    removing unnecessary in MainWindow

--- storage_app/MainWindow.xaml.cs
+++ storage_app/MainWindow.xaml.cs
@@ -1,26 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Newtonsoft.Json;
-using storage_app.Models;
-using storage_app.ViewModels;
-
-using storage_app.Models;
-using storage_app.Services;
-using storage_app.ViewModel;
+﻿using System.Windows;
 
 namespace storage_app
 {
@@ -29,12 +7,8 @@ namespace storage_app
     /// </summary>
     public partial class MainWindow : Window
     {
-        //public MainWindowViewModel MainWindowViewModel;
-
         public MainWindow()
         {
-            //MainWindowViewModel = new MainWindowViewModel();
-
             InitializeComponent();
         }
     }
