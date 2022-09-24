e1a7640e5d74f80adfa1a0dbd1dd6388de681756
BiancaPuertaRocha <biapuertarocha@gmail.com>
2022-09-23 21:54:03 -0300

    category service

---
+++ storage_app/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using storage_app.Models;
+namespace storage_app.Services
+{
+    internal interface ICategoryService
+    {
+        public Task<List<Category>> GetCategories();
+    }
+}
