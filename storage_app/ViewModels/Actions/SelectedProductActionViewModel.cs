080f6af7858b246d70e51b635d9d3c8b69310a61
Hilst <hilstfelipe@gmail.com>
2022-09-25 19:42:59 -0300

    create selected action for product

---
+++ storage_app/ViewModels/Actions/SelectedProductActionViewModel.cs
@@ -0,0 +1,32 @@
+﻿using storage_app.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace storage_app.ViewModels.Actions
+{
+    internal class SelectedProductActionViewModel
+    {
+        private SelectedProduct? _selectedProductCommand;
+        public SelectedProduct SelectedProductCommand
+        {
+            get
+            {
+                if (_selectedProductCommand == null)
+                    _selectedProductCommand = new SelectedProduct(_ => { });
+                return _selectedProductCommand;
+            }
+            set
+            {
+                _selectedProductCommand = value;
+            }
+        }
+    }   
+
+    internal class SelectedProduct : CommandExecutor
+    {
+        public SelectedProduct(Action<object?> action) : base(action) { }
+    }
+}
