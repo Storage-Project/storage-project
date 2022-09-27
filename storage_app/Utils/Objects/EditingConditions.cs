49aa85ce87744b75783ca60f61e382a3351ea5f5
Hilst <hilstfelipe@gmail.com>
2022-09-26 21:55:40 -0300

    object to bind conditions for editing

---
+++ storage_app/Utils/Objects/EditingConditions.cs
@@ -0,0 +1,25 @@
+﻿namespace storage_app.Utils.Objects
+{
+    public class EditingConditions
+    {
+        public bool IsEditing { get; set; } = false;
+        public bool IsAdding { get; set; } = false;
+        public bool IsSavable
+        {
+            get { return IsEditing && !IsAdding; }
+        }
+        public bool IsAddable
+        {
+            get { return IsEditing && IsAdding; }
+        }
+        public bool IsCancelable
+        {
+            get { return IsAddable || IsSavable; }
+        }
+
+        public bool IsNotEditing
+        {
+            get { return !IsEditing; }
+        }
+    }
+}
