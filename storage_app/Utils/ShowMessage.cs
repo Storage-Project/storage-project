4c56f8925876090600ec6d8ca94bce7d1c4f7042
BiancaPuertaRocha <biapuertarocha@gmail.com>
2022-09-28 21:33:35 -0300

    adding utils - show message

---
+++ storage_app/Utils/ShowMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace storage_app.Utils
+{
+    public class ShowMessage
+    {
+        public static void DefaultMessage(string message)
+        {
+            MessageBox.Show(message, "Alert", MessageBoxButton.OK);
+        }
+
+        public static void ErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public static bool QuestionMessage(string message)
+        {
+            MessageBoxResult result = MessageBox.Show(message, "Confirmation", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            return result == MessageBoxResult.OK;
+        }
+    }
+}
